3ea0a2cdf299d5db2942790b7ea086208530685f
bansalaj <bansalaj@vmware.com>
2023-12-27 13:49:03 +0530

    pass 2nd stage stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 // Uncomment this block to pass the first stage
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::{Read, Write, BufReader};
+
+fn handle_client(mut stream: TcpStream) {
+    let mut reader = BufReader::new(&stream);
+    let mut buffer = [0; 1024];
+
+    match reader.read(&mut buffer){
+        Ok(_) => {
+            println!("Received a request!");
+        }
+        Err(e) => println!("Failed to read from connection: {}", e),
+    }
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -15,6 +33,7 @@ fn main() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                handle_client(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
